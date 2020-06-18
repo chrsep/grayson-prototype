89333c585316dcd66fd32c978bcbe2c363f3f8d3
Chrisando Pramudhita <chrsep@protonmail.com>
2020-06-18 22:29:06 +0700

    Add product button.

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,30 @@
 import React, { FC } from "react"
 import Button from "../Button/Button"
 import useGetUserProfileApi from "../../hooks/useGetUserProfileApi"
+import BoxIcon from "../../icons/box.svg"
 
 const Layout: FC = ({ children }) => {
-  const { data, error, status } = useGetUserProfileApi()
+  const { data, status } = useGetUserProfileApi()
 
   return (
     <div>
       <nav className="p-3 flex items-center mx-auto max-w-4xl">
-        <div>Grayson</div>
-        {status !== "success" && (
+        <img alt="profile-pic" src={data?.picture} className="w-8 rounded" />
+        <div className="ml-3">{data?.name}</div>
+        {status !== "success" ? (
           <a href="/api/login" className="ml-auto">
             <Button>Login</Button>
+          </a>
+        ) : (
+          <a href="/product" className="ml-auto">
+            <Button className="flex items-center">
+              <img
+                alt="product icon"
+                src={BoxIcon}
+                className="text-white mr-2 w-5"
+              />
+              Produk
+            </Button>
           </a>
         )}
       </nav>
