08acd4d5c7b7a73e54aae5c28997403c491abf35
Chrisando Pramudhita <chrsep@protonmail.com>
2020-06-20 04:08:22 +0700

    feat: PWA (#29)
    * Implement PWA.
    
    * Remove roboto font.
    
    * Add confirm dialog for update. Remove unecessary codes. Add typescript definition for workbox.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,33 @@
 /* eslint-disable */
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+
+class Workbox {
+  constructor(scriptURL: string, registerOptions?: object);
+
+  register(immediate?: boolean): Promise<any>;
+
+  active(): Promise<ServiceWorker>;
+
+  controlling(): Promise<ServiceWorker>;
+
+  getSW(): Promise<ServiceWorker>;
+
+  messageSW(data: object): Promise<object>;
+
+  addEventListener(event: 'message', callback: (data: IWorkboxEventMessage) => void): void;
+  addEventListener(event: 'installed', callback: (data: IWorkboxEvent) => void): void;
+  addEventListener(event: 'waiting', callback: (data: IWorkboxEventWaiting) => void): void;
+  addEventListener(event: 'controlling', callback: (data: IWorkboxEvent) => void): void;
+  addEventListener(event: 'activated', callback: (data: IWorkboxEvent) => void): void;
+  addEventListener(event: 'redundant', callback: (data: IWorkboxEvent) => void): void;
+  addEventListener(event: 'externalinstalled', callback: (data: IWorkboxEventExternal) => void): void;
+  addEventListener(event: 'externalwaiting', callback: (data: IWorkboxEventExternal) => void): void;
+  addEventListener(event: 'externalactivated', callback: (data: IWorkboxEventExternal) => void): void;
+}
+
+
+interface Window {
+  workbox: Workbox
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
 import "../global.css"
+import { useEffect } from "react"
 import Layout from "../components/Layout/Layout"
 
-const App = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-)
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
+    // NOTE: set skipWaiting to false in next.config.js pwa object
+    // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+    window.workbox.addEventListener("waiting", () => {
+      if (
+        // eslint-disable-next-line no-alert
+        window.confirm(
+          "A new version is installed, reload to use the new version immediately?"
+        )
+      ) {
+        window.workbox.addEventListener("controlling", () => {
+          window.location.reload()
+        })
+        window.workbox.messageSW({ type: "SKIP_WAITING" })
+      } else {
+        // User rejected, new verion will be automatically load when user open the app next time.
+      }
+    })
+  }, [])
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
 
 export default App
