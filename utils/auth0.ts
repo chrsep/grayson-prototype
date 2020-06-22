12570aa9e1bd00b9b5534d629954e9e516b51917
Chrisando Pramudhita <chrsep@protonmail.com>
2020-06-22 21:14:18 +0700

    Fix ts errors.

--- utils/auth0.ts
+++ utils/auth0.ts
@@ -1,15 +1,15 @@
 import { initAuth0 } from "@auth0/nextjs-auth0"
 
 export default initAuth0({
-  domain: process.env.AUTH0_DOMAIN,
-  clientId: process.env.AUTH0_CLIENT_ID,
+  domain: process.env.AUTH0_DOMAIN ?? "",
+  clientId: process.env.AUTH0_CLIENT_ID ?? "",
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: "openid profile email",
-  redirectUri: process.env.AUTH0_REDIRECT_URI,
-  postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
+  redirectUri: process.env.AUTH0_REDIRECT_URI ?? "",
+  postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI ?? "",
   session: {
     // The secret used to encrypt the cookie.
-    cookieSecret: process.env.AUTH0_COOKIE_SECRET,
+    cookieSecret: process.env.AUTH0_COOKIE_SECRET ?? "",
     // The cookie lifetime (expiration) in seconds. Set to 8 hours by default.
     cookieLifetime: 60 * 60 * 24 * 7,
     // (Optional) The cookie domain this should run on. Leave it blank to restrict it to your domain.
