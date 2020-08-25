9e915bd0f17ebc98bc631815b3f5e94b7df43160
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-25 20:06:26 +0700

    remove unused function.

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,7 +1,3 @@
 export const Categories = ["makanan", "pakaian", "seni"] as const
 
 export type Category = typeof Categories[number]
-
-export const getCategoryName = (id: number): Category => {
-  return Categories[id]
-}
