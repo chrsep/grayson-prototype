4c86c42b435d68115c925a70681ed408f38bfa68
Chrisando Pramudhita <chrsep@protonmail.com>
2021-02-08 14:44:12 +0700

    add cart page.

---
+++ src/pages/cart.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react"
+
+const ProfilePage: FC = () => {
+  return (
+    <>
+      <main className="mx-auto max-w-4xl pb-8">
+        <h1 className="mx-4 mt-16 font-bold text-4xl">Keranjang</h1>
+      </main>
+    </>
+  )
+}
+
+export default ProfilePage
